08821f16bde3a290f2edb7bf4885de691733b595
JamesRobertJohns <jyng1503@gmail.com>
2024-09-06 12:41:39 +0800

    updated skeleton functions to work with. Bundled all problem solution in class MergeSort

---
+++ src/helperFunc.h
@@ -0,0 +1,74 @@
+struct MergeSort {
+	MergeSort(vector<int>& A) :
+		n{ A.size() },
+	       	key_cmp{} {}
+			
+	void sort_default(vector<int>& A, int p, int r) {
+		if ( p < r ) {
+			int q  = (p+r) / 2;
+			this->sort_default(A, p, q);
+			this->sort_default(A, q+1, r);
+			merge(A, p, q, r);
+		}
+	}	
+
+	void sort_modified() {
+		 // TODO 
+	}
+
+	void insertion_sort() {
+		// TODO
+	}
+
+	void merge(vector<int>& A, int p, int q, int r) {
+		vector<int> L, R;
+		for (int i = p; i <= q; i++) L.push_back(A[i]);
+		for (int i = q+1; i <= r; i++) R.push_back(A[i]);
+		
+		int idx_L{}, idx_R{}, idx_A{ p };
+		while (idx_L < L.size() && idx_R < R.size() && idx_A < n) {
+			if (L[idx_L] < R[idx_R]) {
+				A[idx_A] = L[idx_L];
+				idx_L++;		
+			}
+			else {
+				A[idx_A] = R[idx_R];
+				idx_R++;
+			}	
+			idx_A++;
+			key_cmp++;
+		}	
+		
+		if (idx_A < n) {
+			if (idx_L < L.size()) {
+				while (idx_L < L.size()) {
+					A[idx_A] = L[idx_L];
+					idx_L++;
+					idx_A++;
+				}	
+			}
+			else{
+				while (idx_R < R.size()) {
+					A[idx_A] = R[idx_R];
+					idx_R++;
+					idx_A++;
+				}
+			}
+		}
+	}
+
+	unsigned long long getKeyCmp() {
+		return key_cmp;
+	}
+
+	private:
+		unsigned n;
+		unsigned long long key_cmp{};
+};
+
+
+void printArray(vector<int>& A) {
+	for (const auto i : A)
+		cout << i << ' ';
+	cout <<  endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
+#include<vector>
+
+using namespace std;
+
+#include "helperFunc.h"
 
 int main() {
+	vector<int> TEST = { 1, 24, 211, 12, 10, 1923, 10, 10000, 129, 129, 129, 45 };
+
+	MergeSort test{ TEST };
+	cout << "unsorted test array: " << endl;
+	printArray(TEST);
+	cout << "number of key comparisons: " << test.getKeyCmp() << endl;
+	cout << endl;
+
+	test.sort_default(TEST, 0, TEST.size()-1);
+	cout << "sorted test array: " << endl;
+	printArray(TEST);
+	cout << "number of key comparisons: " << test.getKeyCmp() << endl;
+	
 	return 0;
 }
